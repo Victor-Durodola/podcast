5174d4731ca4d563737b1277a704b278944d9d31
Victor-Durodola <78563513+Victor-Durodola@users.noreply.github.com>
2023-09-27 09:08:19 -0500

    add: initial files

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/podcast",
-  plugins: [react()],
+// export default defineConfig({
+//   base: "/podcast",
+//   plugins: [react()],
+// })
+
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/podcast/'
+  }
+
+  return config
 })
